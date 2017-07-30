cbf4dcb02d6e6c427fd56161a444c3af6e501ac0
Hari Kishore <harikishore96@gmail.com>
2017-07-31 00:15:54 +0530

    UPDATE: Gulpfile updated to build source and get files ready for distribution.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,14 @@
 var gulp = require('gulp');
+const imagemin = require('gulp-imagemin');
+const useref = require('gulp-useref');
+const gulpif = require('gulp-if');
+const uglify = require('gulp-uglify');
+const minifyCss = require('gulp-clean-css');
 var browserSync = require('browser-sync').create();
 
 var path = ['src/index.html', 'src/app.module.js'];
+
+var images = ['src/asserts/brand.jpg', 'src/asserts/logo.png'];
 
 //Watch files
 gulp.task('watch', function(done) {
@@ -10,7 +17,7 @@ gulp.task('watch', function(done) {
 });
 
 // Static server
-gulp.task('serve', function() {
+gulp.task('serve:src', function() {
     browserSync.init({
         server: {
             baseDir: 'src/'
@@ -19,4 +26,28 @@ gulp.task('serve', function() {
     gulp.watch(path, ['watch']);
 });
 
-gulp.task('default', ['serve']);
+gulp.task('imagemin', function() {
+    gulp.src(images)
+        .pipe(imagemin())
+        .pipe(gulp.dest('dist/asserts'));
+});
+
+gulp.task('useref', function() {
+    gulp.src('src/index.html')
+        .pipe(useref())
+        .pipe(gulpif('*.js', uglify()))
+        .pipe(gulpif('*.css', minifyCss()))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('serve:dist', function() {
+    browserSync.init({
+        server: {
+            baseDir: 'dist/'
+        }
+    });
+});
+
+gulp.task('build', ['imagemin', 'useref', 'serve:dist']);
+
+gulp.task('default', ['serve:src']);
